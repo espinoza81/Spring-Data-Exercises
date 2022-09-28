a8e43fd802728cd4dfe5b0714669e70d15297524
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-09-28 17:06:03 +0300

    Create E01_Outputs.java

---
+++ 01 Introduction To DB Apps/E01_Outputs.java
@@ -0,0 +1,25 @@
+public class E01_Outputs {
+    static final String E03_ENTER_ID = "Enter villain id: ";
+
+    static final String E03_PRINT_VILLAIN_NAME = "Villain: %s%n";
+
+    static final String E03_PRINT_MINIONS_INFO = "%d. %s %d%n";
+
+    static final String E03_PRINT_NO_VILLAIN_WHIT_ID = "No villain with ID %d exists in the database.%n";
+
+    static final String E04_SUCCESSFULLY_ADDED_MINION_TO_VILLAIN = "Successfully added %s to be minion of %s%n";
+
+    static final String E04_SUCCESSFULLY_ADDED_VILLAIN = "Villain %s was added to the database.%n";
+
+    static final String E04_SUCCESSFULLY_ADDED_TOWN = "Town %s was added to the database.%n";
+
+    static final String E05_COUNT_TOWNS_AFFECTED = " town names were affected.";
+
+    static final String E06_SUCCESSFULLY_DELETED_VILLAIN = "%s was deleted%n";
+
+    static final String E06_NO_VILLAIN_WHIT_ID = "No such villain was found";
+
+    static final String E06_SUCCESSFULLY_RELEASED_MINIONS = "%d minions released%n";
+
+    static final String E09_PRINT_NO_MINION_WHIT_ID = "No minion with ID %d exists in the database.%n";
+}
